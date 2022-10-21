0e2639b1924aa74ce130e262ff815b9e4476f1c4
Bharat Patidar <pritamgam786@gmail.com>
2022-10-21 18:53:48 +0530

    err

--- src/screen/Playground/CodeEditor.tsx
+++ src/screen/Playground/CodeEditor.tsx
@@ -1,18 +1,18 @@
 import React,{useState} from 'react'
 import CodeMirror from "@uiw/react-codemirror";
 //themes
-import {githubDark,githubLight} from "@uiw/codemirror-theme-github";
-import {okaidia} from "@uiw/codemirror-theme-okaidia";
-import {xcodeDark,xcodeLight} from "@uiw/codemirror-theme-xcode";
-import {darcula} from "@uiw/codemirror-theme-darcula";
-import {bespin} from"@uiw/codemirror-theme-bespin";
-import {duotoneLight,duotoneDark} from "@uiw/codemirror-theme-duotone";
-//languages
-import {cpp} from "@codemirror/lang-cpp";
-import {java} from "@codemirror/lang-java";
-import {javascript} from "@codemirror/lang-javascript";
-import {python} from "@codemirror/lang-python";
-//configuration
+import {githubDark} from "@uiw/codemirror-theme-github";
+// import {okaidia} from "@uiw/codemirror-theme-okaidia";
+// import {xcodeDark,xcodeLight} from "@uiw/codemirror-theme-xcode";
+// import {darcula} from "@uiw/codemirror-theme-darcula";
+// import {bespin} from"@uiw/codemirror-theme-bespin";
+// import {duotoneLight,duotoneDark} from "@uiw/codemirror-theme-duotone";
+// //languages
+// import {cpp} from "@codemirror/lang-cpp";
+import{java} from "@codemirror/lang-java";
+// import {javascript} from "@codemirror/lang-javascript";
+// import {python} from "@codemirror/lang-python";
+// //configuration
 import {indentUnit} from "@codemirror/language";
 import {EditorState} from "@codemirror/state";
 import styled from 'styled-components';
@@ -26,7 +26,7 @@ const CodeEditorContainer = styled.div`
 `
 const CodeEditor = () => {
     const[theme,setTheme] = useState<any>(githubDark);
-    const[lang,setLang] = useState<any>(python);
+    const[lang,setLang] = useState<any>(java);
   return (
     <CodeEditorContainer>
         <CodeMirror
